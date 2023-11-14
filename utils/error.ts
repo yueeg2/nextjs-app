448bdfdf874a67cb9199584bf79b1806fe5f73ee
yue <yuesu198@gmail.com>
2023-11-15 01:24:39 +0800

    fix: Cannot find module

--- utils/error.ts
+++
@@ -1,8 +0,0 @@
-export const resolve = (_err: any) => new Response(
-  JSON.stringify({ error: 'failed to load data' }),
-  {
-    status: 500,
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
