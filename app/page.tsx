7df580c2ea3b8c7e8e959ac14b176ad7847a6a3e
yue <yuesu198@gmail.com>
2023-11-16 12:15:11 +0800

    style: add reset.css

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Main from './_main'
 export default function Home() {
 
-  return <main className="flex min-h-screen flex-col items-center p-24">
+  return <main className="flex min-h-screen flex-col items-center p-24 gap-[4rem]">
     <Main />
   </main>
 
